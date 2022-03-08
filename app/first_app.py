8bcc85a09b86d11814786cc78dca20c97debb82a
AlexTwenji777 <alextwinje@gmail.com>
2022-03-08 20:05:51 +0300

    Fixing deprecated streamlit commands

--- app/first_app.py
+++ app/first_app.py
@@ -32,12 +32,12 @@ option = st.sidebar.selectbox("Which number do you like best?", df["first column
 
 "You selected: ", option
 
-left_column, right_column = st.beta_columns(2)
+left_column, right_column = st.columns(2)
 pressed = left_column.button("Press me?")
 if pressed:
     right_column.write("Woohoo!")
 
-expander = st.beta_expander("FAQ")
+expander = st.expander("FAQ")
 expander.write("Here you could put in some really, really long explanations...")
 
 'Starting a long computation...'
